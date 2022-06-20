9cf278ec5121f8e2ade2f888e2c397c34d80af77
Gaurav <1996gauravb@gmail.com>
2022-06-20 16:38:17 +0530

    updating commands on user file and adding sed command to add redis and mongodb ip

--- components/user.sh
+++ components/user.sh
@@ -1,1 +1,22 @@
-echo Install User Component
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+
+rm -rf user
+
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+
+
+sed -i -e 's/REDIS_ENDPOINT/redis.dailypractice.internal/' -e 's/MONGO_ENDPOINT/mongodb.dailypractice.internal' /home/roboshop/user/systemd.service
+
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
