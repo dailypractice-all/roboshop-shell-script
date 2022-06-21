01d0eb343b1420ccb8a72671d506ca1f96a4faa5
Gaurav <1996gauravb@gmail.com>
2022-06-21 18:20:17 +0530

    removing sed

--- components/shipping.sh
+++ components/shipping.sh
@@ -11,7 +11,7 @@ cd shipping
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-sed -i -e 's/CARTENDPOINT/cart.dailypractice.internal/' -e 's/DBHOST/mysql.dailypractice.internal/' /home/roboshop/shipping/systemd.service
+
 mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
 systemctl daemon-reload
 systemctl restart shipping
