fdb3647eda7a3244c36c505b26b928f5bc343c22
Gaurav <1996gauravb@gmail.com>
2022-06-21 15:31:48 +0530

    adding mysql default password

--- components/mysql.sh
+++ components/mysql.sh
@@ -3,6 +3,11 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
+MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
+# HERE TO SET THE DEFAULT PASSWORD RUN SUDO COMMAND WITH (grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}') AND WILL GET THE TEMPORARY PASSWORD AND AWK IS USED TO GET PASSWORD TFROM COULMN AND $ NF IS USED FOR NTH FEILD IF WE USE $ THEN IT COLLRILATES TO COLUMN 1..... IT CONCLUED THAT IF WE RUN SOME COMMAND THE OUTPUT WHICH COMES IT GOES TO THE VARIABLE.
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >tmp/mysql
+mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" >tmp/mysql
 
 
 # /var/log/mysqld.log
