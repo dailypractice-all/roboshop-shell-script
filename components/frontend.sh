9496bf2753f085a93e5527846d81978db11c535d
Gaurav <1996gauravb@gmail.com>
2022-06-21 13:50:26 +0530

    adding sed command for catalogue, user, cart

--- components/frontend.sh
+++ components/frontend.sh
@@ -13,3 +13,4 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+sed -i -e '/catalogue/ s/localhost/catalogue.dailypractice.internal/' -e '/user/ s/localhost/user.dailypractice.internal/ -e '/cart/ s/localhost/cart.dailypractice.internal/' /etc/nginx/default.d/roboshop.conf
