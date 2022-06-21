26ebb0c61ec662f070fdc90dd78fa5d722629e35
Gaurav <1996gauravb@gmail.com>
2022-06-21 13:25:19 +0530

    editing cart.sh file with command and adding rm -rf cart to remove cart file and adding sed command to put ip os redis and catalogue

--- components/cart.sh
+++ components/cart.sh
@@ -1,3 +1,22 @@
-echo Install Cart Component
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
 
-# command kindly install all .sh file to linux by command bash roboshop.sh frontend
+useradd roboshop
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.dailypractice.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.dailypractice.internal/' /home/roboshop/cart/systemd.service
+
+Update `REDIS_ENDPOINT` with REDIS server IP Address
+Update `CATALOGUE_ENDPOINT` with Catalogue server IP address
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl start cart
+systemctl enable cart
