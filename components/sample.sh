d4e2963f0cae9ca217a717a3b41d4809ff345ed0
Gaurav <1996gauravb@gmail.com>
2022-06-22 17:26:53 +0530

    finding smallest prime number

--- components/sample.sh
+++ components/sample.sh
@@ -1,53 +1,14 @@
-#include <bits/stdc++.h>
+#!bin/bash
 
-using namespace std;
+read -p "Enter Number: " num
+if [ $num % 2 -eq 0 ]
+then
+  echo "Even number"
 
-bool isPrime(char c) {
-	return c == '2' || c == '3' || c == '5' || c == '7';
-}
+elif [ $num % 2 -ne 0 ]
+then
+  echo "Odd number"
 
-void decrease(string& n, int i) {
-	if (n[i] <= '2') {
-		n.erase(i, 1);
-		n[i] = '7';
-	}else if (n[i] == '3') {
-		n[i] = '2';
-    }else if (n[i] <= '5') {
-		n[i] = '3';
-    }else if (n[i] <= '7') {
-		n[i] = '5';
-    }else {
-		n[i] = '7';
-    }
-	return;
-}
-
-string getPrimeDigitsNumber(string n) {
-	for (int i = 0; i < n.length(); i++) {
-
-		if (!isPrime(n[i])) {
-
-			while (n[i] <= '2' && i >= 0) {
-				i--;
-            }
-
-			if (i < 0) {
-				i = 0;
-			}
-            decrease(n, i);
-
-			for (int j = i + 1; j < n.length(); j++) {
-				n[j] = '7';
-            }
-			break;
-		}
-	}
-
-	return n;
-}
-
-int main() {
-	string n = "7464";
-	cout << getPrimeDigitsNumber(n) << endl;
-	return 0;
-}
+else
+  echo "nither even nor odd"
+fi
