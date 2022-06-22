b770dfe891b003105d2057afc39b034a82b238f6
Gaurav <1996gauravb@gmail.com>
2022-06-22 17:17:38 +0530

    finding smallest prime number

--- components/sample.sh
+++ components/sample.sh
@@ -1,4 +1,25 @@
 #!bin/bash
+
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(char c) {
+   return c == '2' || c == '3' || c == '5' || c == '7';
+}
+void decrease(string& n, int i) {
+   if (n[i] <= '2') {
+      n.erase(i, 1);
+      n[i] = '7';
+   }else if (n[i] == '3') {
+      n[i] = '2';
+   }else if (n[i] <= '5') {
+      n[i] = '3';
+   }else if (n[i] <= '7') {
+      n[i] = '5';
+   }else {
+      n[i] = '7';
+   }
+   return;
+}
 string getPrimeDigitsNumber(string n) {
    for (int i = 0; i < n.length(); i++) {
       if (!isPrime(n[i])) {
