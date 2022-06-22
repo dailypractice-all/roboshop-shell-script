c01b354ad7ace51332aa9d5767289db12bdee4af
Gaurav <1996gauravb@gmail.com>
2022-06-22 17:15:28 +0530

    finding smallest prime number

--- components/sample.sh
+++ components/sample.sh
@@ -1,8 +1,24 @@
 #!bin/bash
-
-
-if ( n % 2 )
-  echo printf ( “It’s an odd number” );
-else
-  printf ( “It’s an even number” );
-fi
+string getPrimeDigitsNumber(string n) {
+   for (int i = 0; i < n.length(); i++) {
+      if (!isPrime(n[i])) {
+         while (n[i] <= '2' && i >= 0) {
+            i--;
+         }
+         if (i < 0) {
+            i = 0;
+         }
+         decrease(n, i);
+         for (int j = i + 1; j < n.length(); j++) {
+            n[j] = '7';
+         }
+         break;
+      }
+   }
+   return n;
+}
+int main() {
+   string n = "7464";
+   cout << getPrimeDigitsNumber(n) << endl;
+   return 0;
+}
