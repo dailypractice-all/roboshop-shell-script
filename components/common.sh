b5b3c491d09c78be177d1f74c894e431dba54431
Gaurav <1996gauravb@gmail.com>
2022-06-23 12:54:16 +0530

    MAKING CART FILE MORE DRY

--- components/common.sh
+++ components/common.sh
@@ -24,3 +24,58 @@ PRINT() {
   echo "------------$1----------" >>${LOG}
   echo "$1"
 }
+
+NODEJS() {
+
+  CHECK_ROOT
+
+  PRINT "setting up NodeJS YUM Repo is "
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Installing Nodejs"
+  yum install nodejs -y &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Creating Application User "
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  CHECK_STAT $?
+
+  PRINT "Downloading content of ${COMPONENT} "
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG}
+  CHECK_STAT $?
+
+  cd /home/roboshop
+
+  PRINT "Removing all content "
+  rm -rf ${COMPONENT} &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Extract ${COMPONENT} Zip "
+  unzip /tmp/${COMPONENT}.zip &>>${LOG}
+  CHECK_STAT $?
+
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
+
+  PRINT "Installing ${COMPONENT} dependencies for ${COMPONENT} component "
+  npm install &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Updating SystemD Configuraton"
+  sed -i -e 's/REDIS_ENDPOINT/redis.dailypractice.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.dailypractice.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
+  CHECK_STAT $?
+
+  PRINT "Setup SystemD Configuraton "
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG}
+  CHECK_STAT $?
+
+  systemctl daemon-reload
+  systemctl restart ${COMPONENT}
+
+  PRINT "Start Cart Service "
+  systemctl enable ${COMPONENT} &>>${LOG}
+  CHECK_STAT $?
